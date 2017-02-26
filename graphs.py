88c88db2af7037eba0b7c9c3f22fbf0608527bb5
Jhonatan Oliveira <jhonatanoliveira@gmail.com>
2017-02-25 21:50:29 -0600

    extend graph class

--- graphs.py
+++ graphs.py
@@ -1,11 +1,15 @@
 class Graph:
 
-  def __init__(self, edges=[], directed=True):
+  def __init__(self, edges=[], directed=True, root=None):
     
     self.directed = directed
     self.graph = {}
-    self.add_edges_from(edges)
+    
+    self.root = root
+    if root:
+      self.graph[tuple(root)] = set([])
   
+    self.add_edges_from(edges)
 
   def add_edges_from(self, edges):
     for node1, node2 in edges:
@@ -15,10 +19,12 @@ class Graph:
   def add_edge(self, node1, node2):
 
     def _add(n1, n2):
+      n1 = tuple(n1)
+      n2 = tuple(n2)
       if n1 in self.graph.keys():
         self.graph[n1].add(n2)
       else:
-        self.graph[n1] = set()
+        self.graph[n1] = set([n2])
 
     _add(node1, node2)
 
